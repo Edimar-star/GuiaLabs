17bc40bbb56407956ea538d1c2c5b821922ab66a
Edilberto Mario Rodríguez Fontalvo <edimarod@outlook.es>
2023-04-30 19:09:24 -0500

    fixed

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  PORT: process.env.PORT || 3000,
-  DB_USER: process.env.DB_USER || 'Edilberto',
-  DB_PASSWORD: process.env.DB_PASSWORD || 'NNzb0N4PqMlkVte3',
-  DB_HOST: process.env.DB_HOST || 'cluster0.ru7drgq.mongodb.net',
-  DB_DATABASE: process.env.DB_DATABASE || 'favoriteProducts',
+  PORT: process.env.PORT,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_HOST: process.env.DB_HOST,
+  DB_DATABASE: process.env.DB_DATABASE,
 };
